de73eb449bd143dcfa7be571ff44c10b6303f5fa
Polina Raguilo <Palina.Rahila@itechart-group.com>
2021-05-14 17:16:59 +0300

    Added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Switch } from "react-router";
 import "./App.css";
+import Header from "./components/header/header";
 import HomePage from "./pages/home-page/home-page";
 import ShopPage from "./pages/shop-page/shop-page";
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage}/>
---
+++ src/components/header/header.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import './header.scss';
+import {ReactComponent as Logo} from '../../assets/crown.svg'
+
+const Header = () => {
+    return (
+      <div className="header">
+        <Link to="/" className="logo-comtainer">
+          <Logo className="logo" />
+        </Link>
+        <div className="options">
+          <Link to="/shop" className="option">
+            SHOP
+          </Link>
+          <Link to="/shop" className="option">
+            CONTACT
+          </Link>
+        </div>
+      </div>
+    );
+}
+
+export default Header
