d8e7c86b1a4453574ddff3d5162a5fc9efe770ab
Polina Raguilo <Palina.Rahila@itechart-group.com>
2021-05-19 10:29:01 +0300

    Created checkout- page and checkout-item

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,21 +1,26 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import { toggleCartHidden } from '../../redux/cart/cart-actions';
 import { selectCartItems } from '../../redux/cart/cart-selectors';
 import CartItem from '../cart-item/cart-item';
 import CustomButton from '../custom-button/custom-button';
 import './cart-dropdown.scss';
 
-const CartDropdown = ({ cartItems, history }) => {
+const CartDropdown = ({ cartItems, history, hideCart }) => {
+  const onClickHandler = () => {
+    history.push('/checkout');
+    hideCart();
+  };
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
         {cartItems.length !== 0 ? (
           cartItems.map((item) => <CartItem key={item.id} item={item} />)
         ) : (
-          <span className="empty-message">you cart is empty</span>
+          <span className="empty-message">Your cart is empty</span>
         )}
       </div>
-      <CustomButton inverted onClick={() => history.push('/checkout')}>
+      <CustomButton inverted onClick={onClickHandler}>
         go to checkout
       </CustomButton>
     </div>
@@ -28,4 +33,12 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default withRouter(connect(mapStateToProps, null)(CartDropdown));
+const mapDispatchToProps = (dispatch) => {
+  return {
+    hideCart: () => dispatch(toggleCartHidden()),
+  };
+};
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(CartDropdown),
+);
--- src/pages/checkout-page/checkout-page.js
+++ src/pages/checkout-page/checkout-page.js
@@ -1,7 +1,46 @@
+import { connect } from 'react-redux';
+import CheckoutItem from '../../components/checkout-item/checkout-item';
+import {
+  selectCartItems,
+  selectCartTotal,
+} from '../../redux/cart/cart-selectors';
 import './checkout-page.scss';
 
-const CheckoutPage = () => {
-  return <div> checkout page</div>;
+const CheckoutPage = ({ cartItems, totalPrice }) => {
+  return (
+    <div className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map((cartItem) => {
+        return <CheckoutItem key={cartItem.id} cartItem={cartItem} />;
+      })}
+      <div className="total">
+        <span>Total: ${totalPrice}</span>
+      </div>
+    </div>
+  );
 };
 
-export default CheckoutPage;
+const mapStateToProps = (state) => {
+  return {
+    cartItems: selectCartItems(state),
+    totalPrice: selectCartTotal(state),
+  };
+};
+
+export default connect(mapStateToProps, null)(CheckoutPage);
